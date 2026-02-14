7cbf2a827346d44c2231db42fdc6c8e480583486
Mia Vogel <joschi0317mail.com>
2026-02-14 16:15:07 +0100

    added delete-icon

--- app.js
+++ app.js
@@ -35,7 +35,19 @@ function renderLists() {
     li.textContent = list.name;
 
     const delBtn = document.createElement("button");
-    delBtn.textContent = "Löschen";
+    delBtn.innerHTML = `
+<svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem"
+viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+stroke-linecap="round" stroke-linejoin="round">
+  <path d="M3 6h18"/>
+  <path d="M8 6V4h8v2"/>
+  <path d="M19 6l-1 14H6L5 6"/>
+  <path d="M10 11v6"/>
+  <path d="M14 11v6"/>
+</svg>
+`;
+
+
     delBtn.classList.add("delete-btn");
 
 
@@ -91,7 +103,17 @@ function renderTasks() {
     span.textContent = task.title;
 
     const delBtn = document.createElement("button");
-    delBtn.textContent = "Löschen";
+    delBtn.innerHTML = `
+<svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem"
+viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+stroke-linecap="round" stroke-linejoin="round">
+  <path d="M3 6h18"/>
+  <path d="M8 6V4h8v2"/>
+  <path d="M19 6l-1 14H6L5 6"/>
+  <path d="M10 11v6"/>
+  <path d="M14 11v6"/>
+</svg>
+`;
     delBtn.classList.add("delete-btn");
 
 
