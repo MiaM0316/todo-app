20f9095ee516b71fb0408a24209336706946416b
Mia Vogel <joschi0317mail.com>
2026-02-14 13:31:04 +0100

    made tasks visible, addable and deletable

--- app.js
+++ app.js
@@ -43,6 +43,8 @@ const tasksUl = document.getElementById("tasks");
 const currentListTitle = document.getElementById("current-list-title");
 
 const addListBtn = document.getElementById("add-list-btn");
+const addTaskBtn = document.getElementById("add-task-btn");
+
 
 
 function renderLists() {
@@ -57,13 +59,43 @@ function renderLists() {
       btn.addEventListener("click", () => {
         state.selectedListId = list.id;
        saveState();
-       renderSelectedListTitle();
+        renderSelectedListTitle();
+        renderTasks();
     });
 
     li.appendChild(btn);
     listsUl.appendChild(li);
   }
 }
+
+function renderTasks() {
+  tasksUl.innerHTML = "";
+
+  const selected = state.lists.find(l => l.id === state.selectedListId);
+  if (!selected) return;
+
+  for (const task of selected.tasks) {
+  const li = document.createElement("li");
+
+  const span = document.createElement("span");
+  span.textContent = task.title;
+
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "Löschen";
+
+  delBtn.addEventListener("click", () => {
+    selected.tasks = selected.tasks.filter(t => t.id !== task.id);
+    saveState();
+    renderTasks();
+  });
+
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  tasksUl.appendChild(li);
+}
+
+}
+
 
 function renderSelectedListTitle() {
   const selected = state.lists.find(l => l.id === state.selectedListId);
@@ -95,6 +127,30 @@ addListBtn.addEventListener("click", () => {
   renderSelectedListTitle();
 });
 
+addTaskBtn.addEventListener("click", () => {
+  const selected = state.lists.find(l => l.id === state.selectedListId);
+  if (!selected) {
+    alert("Bitte zuerst eine Liste auswählen.");
+    return;
+  }
+
+  const title = prompt("Neue Aufgabe:");
+  if (!title) return;
+
+  const trimmed = title.trim();
+  if (!trimmed) return;
+
+  selected.tasks.push({
+    id: crypto.randomUUID(),
+    title: trimmed,
+    done: false
+  });
+
+  saveState();
+  renderTasks();
+});
+
+
 
 
 
@@ -102,6 +158,7 @@ loadState();
 initStateIfEmpty();
 renderLists();
 renderSelectedListTitle();
+renderTasks();
 
 
 console.log("Geladener State:", state);
