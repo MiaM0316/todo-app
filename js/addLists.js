f34d10e48632256f1a89d242627205376c89372c
Mia Vogel <joschi0317mail.com>
2026-02-18 17:31:09 +0100

    added new UI for adding Lists

--- js/addLists.js
+++ js/addLists.js
@@ -2,15 +2,36 @@ import { state } from "./state.js";
 import {saveState } from "./storage.js";
 import { renderLists } from "./lists.js";
 
+const ListsUl = document.getElementById("lists");
 const addListBtn = document.getElementById("add-list-btn");
 
-addListBtn.addEventListener("click", () => {
-  const name = prompt("Name der neuen Liste:");
-  if (!name) return;
+addListBtn.addEventListener("click", showListInput);
+
+function showListInput() {
+  const li = document.createElement("li");
+  const input = document.createElement("input");
+
+  input.type = "text";
+  input.placeholder = "Liste...";
+  input.classList.add("list-input");
+
+  li.appendChild(input);
+  ListsUl.appendChild(li);
+
+  input.focus();
+
+  let saved = false;
+  let cancelled = false;
+
+  function saveIfNeeded() {
+  if (saved) return;
+
+  const value = input.value.trim();
+  if (!value) return;
 
   const newList = {
     id: crypto.randomUUID(),
-    name: name.trim(),
+    name: value.trim(),
     tasks: []
   };
 
@@ -18,5 +39,23 @@ addListBtn.addEventListener("click", () => {
   state.selectedListId = newList.id;
 
   saveState();
-  renderLists();
-});
+  saved = true;
+  }
+  
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      saveIfNeeded();
+      renderLists();
+    }
+    else if (e.key === "Escape") {
+      cancelled = true;
+      renderLists();
+    }
+  });
+
+  input.addEventListener("blur", () => {
+      if (cancelled) return;
+    saveIfNeeded();
+    renderLists();
+  });
+};
