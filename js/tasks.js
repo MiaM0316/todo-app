95e68f1889e83b26fa7b24ce15cc2507e05b1d26
Mia Vogel <joschi0317mail.com>
2026-02-20 16:03:56 +0100

    added editBtn Mask to lists and tasks

--- js/tasks.js
+++ js/tasks.js
@@ -34,18 +34,29 @@ export function renderTasks() {
     const span = document.createElement("span");
     span.textContent = task.title;
 
+    const editBtn = document.createElement("button");
+    editBtn.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem"
+    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M12 20h9"/>
+      <path d="M16.5 3.5a2.1 2.1 0 0 1 3 3L7 19l-4 1 1-4Z"/>
+    </svg>
+    `;
+    editBtn.classList.add("edit-btn");
+
     const delBtn = document.createElement("button");
     delBtn.innerHTML = `
-<svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem"
-viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-stroke-linecap="round" stroke-linejoin="round">
-  <path d="M3 6h18"/>
-  <path d="M8 6V4h8v2"/>
-  <path d="M19 6l-1 14H6L5 6"/>
-  <path d="M10 11v6"/>
-  <path d="M14 11v6"/>
-</svg>
-`;
+    <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem"
+    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+    stroke-linecap="round" stroke-linejoin="round">
+      <path d="M3 6h18"/>
+      <path d="M8 6V4h8v2"/>
+      <path d="M19 6l-1 14H6L5 6"/>
+      <path d="M10 11v6"/>
+      <path d="M14 11v6"/>
+    </svg>
+    `;
     delBtn.classList.add("delete-btn");
 
 
@@ -57,6 +68,7 @@ stroke-linecap="round" stroke-linejoin="round">
     });
     li.appendChild(checkbox);
     li.appendChild(span);
+    li.appendChild(editBtn);
     li.appendChild(delBtn);
     tasksUl.appendChild(li);
 }}
